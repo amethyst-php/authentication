d322b5b39fc21f7fc0c6e807800078ff80ec16e5
railken <railken@protonmail.ch>
2018-06-27 00:50:50 +0200

    fix missing trait passport

--- src/Concerns/Auth/User.php
+++ src/Concerns/Auth/User.php
@@ -7,10 +7,11 @@ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 use Railken\LaraOre\User\User as BaseUser;
+use Laravel\Passport\HasApiTokens;
 
 class User extends BaseUser implements AuthenticatableContract, AuthorizableContract
 {
-    use Authenticatable, Authorizable;
+    use Authenticatable, Authorizable, HasApiTokens;
 
     /**
      * Retrieve user for passport oauth.
