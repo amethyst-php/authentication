08348f87b74e92fe2f593e5b71ab3e5a2d3ca4dc
railken <railken@protonmail.ch>
2018-06-27 00:50:55 +0200

    update tests

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -3,9 +3,12 @@
 namespace Railken\LaraOre\Auth\Tests;
 
 use Illuminate\Support\Facades\Config;
+use Railken\LaraOre\Support\Testing\ApiTestableTrait;
 
 class ApiTest extends BaseTest
 {
+    use ApiTestableTrait;
+    
     /**
      * Retrieve basic url.
      *
@@ -27,7 +30,7 @@ class ApiTest extends BaseTest
             'username' => 'admin@admin.com',
             'password' => 'vercingetorige',
         ]);
-        $response->assertStatus(200);
+        $this->assertOrPrint($response, 200);
 
         $access_token = json_decode($response->getContent())->data->access_token;
         $this->withHeaders(['Authorization' => 'Bearer '.$access_token]);
