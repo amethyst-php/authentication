816624047d4c12363fdef6bdc98453049cdd0b7a
railken <railken@protonmail.ch>
2019-08-20 02:05:21 +0200

    BaseUser => Model

--- src/Concerns/Auth/User.php
+++ src/Concerns/Auth/User.php
@@ -2,7 +2,7 @@
 
 namespace Amethyst\Concerns\Auth;
 
-use Amethyst\Models\User as BaseUser;
+use Amethyst\Models\User as Model;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Auth\Access\Authorizable;
 use Laravel\Passport\HasApiTokens;
 use Railken\Lem\Contracts\AgentContract;
 
-class User extends BaseUser implements AuthenticatableContract, AuthorizableContract, AgentContract
+class User extends Model implements AuthenticatableContract, AuthorizableContract, AgentContract
 {
     use Authenticatable;
     use Authorizable;
